1b7b0ed592cc218c3141d1ee9ddb424ae48a1ccf
haalven <147269352+haalven@users.noreply.github.com>
2026-03-05 10:44:16 +0100

    Improve error message clarity in get_url.py

--- get_url.py
+++ get_url.py
@@ -15,14 +15,14 @@ def get_url(url:str, query:dict) -> tuple[list, str]:
             url += '?' + urllib.parse.urlencode(query)
         headers = {'User-Agent': 'curl'} # required by some APIs
         req = urllib.request.Request(url, headers=headers)
-        try:
+        try: # http connection
             with urllib.request.urlopen(req) as response:
                 headers = response.headers.items() # list
                 payload = response.read().decode('utf-8') # str
                 return (headers, payload)
         except urllib.error.HTTPError as e:
-            print('http error:', str(e))
+            print('HTTP error:', str(e))
             return ([],'')
     except Exception as e:
-        print('urllib exception:', str(e))
+        print('URL error:', str(e))
         return ([],'')
