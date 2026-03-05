19da88ee4d202bc6e205d66595ec085a9761ca2d
haalven <147269352+haalven@users.noreply.github.com>
2026-03-05 10:24:21 +0100

    Refactor get_url function with type hints
    Refactor get_url function to improve type hints and error handling.

--- get_url.py
+++ get_url.py
@@ -1,25 +1,28 @@
-#!/usr/bin/env python3
-
 import urllib.parse
 import urllib.request
 import urllib.error
 
+import json
 
-def get_url(url:str, query:dict): # returns Tuple (List, Str) or None
-    try:
+
+def get_url(url:str, query:dict) -> tuple[list, str]:
+    if not url.strip(): # doesn't work
+        return ([],'')
+    if not '://' in url: # try https
+        url = 'https://' + url
+    try: # urllib
         if query: # append to URL
             url += '?' + urllib.parse.urlencode(query)
         headers = {'User-Agent': 'curl'} # required by some APIs
         req = urllib.request.Request(url, headers=headers)
         try:
             with urllib.request.urlopen(req) as response:
-                headers = response.headers.items() # List
-                payload = response.read().decode('utf-8') # Str
-                #print('response headers:', headers)
+                headers = response.headers.items() # list
+                payload = response.read().decode('utf-8') # str
                 return (headers, payload)
         except urllib.error.HTTPError as e:
-            print(e)
+            print('http error:', str(e))
+            return ([],'')
     except Exception as e:
-        print('urlopen exception:', e)
-        return None
-
+        print('urllib exception:', str(e))
+        return ([],'')
